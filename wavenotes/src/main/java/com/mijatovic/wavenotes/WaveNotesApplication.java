916e64d921f9bc9a0a66c72cffd915b5014645c2
Vanja Mijatovic <vmijatovic2000@gmail.com>
2023-04-09 14:07:23 +0200

    Created basic Spring Boot project files

---
+++ wavenotes/src/main/java/com/mijatovic/wavenotes/WaveNotesApplication.java
@@ -0,0 +1,13 @@
+package com.mijatovic.wavenotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaveNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaveNotesApplication.class, args);
+	}
+
+}
